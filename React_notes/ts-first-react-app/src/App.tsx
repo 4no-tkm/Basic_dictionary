271a6f076a7e942685a30539e99bf79d8e18f36d
4no-tkm <takumi.7777@icloud.com>
2025-06-19 22:04:15 +0900

    feat: Setup initial React project with Vite and basic JSX example

--- React_notes/ts-first-react-app/src/App.tsx
+++ React_notes/ts-first-react-app/src/App.tsx
@@ -1,40 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Greeting from './components/Greeting';
+import React from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const currentHour = new Date().getHours();
+  let greetingMessage = "";
+
+  if (currentHour < 12) {
+    greetingMessage = "Good Morning!";
+  } else if (currentHour < 18) {
+    greetingMessage = "Hello!";
+  } else {
+    greetingMessage = "Good Evening!"
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
-      {/* Greeting コンポーネントを使用し、Propsを渡す */}
-      <Greeting name="Student"/>
-      <Greeting name="Instructor" message="Welcome to React!" />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>{greetingMessage} Wellcome to React!</h1>
+      <p>Date: {new Date().toLocaleTimeString()}</p>
+      <p>This is simple application written in React and TypeScript.</p>
+      <hr />
+      <p>JSX practice.</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
