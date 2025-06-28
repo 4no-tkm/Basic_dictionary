57ff8f9d15606700082d035a0978a5a3bed1fe65
4no-tkm <takumi.7777@icloud.com>
2025-06-29 03:09:40 +0900

    refactor: Switch Rails practice project database from SQLite3 to PostgreSQL

--- Rails_notes/rails_practice_api/db/schema.rb
+++ Rails_notes/rails_practice_api/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_06_28_124036) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
